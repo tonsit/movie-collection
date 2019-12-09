9e3df69cc93971b339c443241dbbe93e28f00661
Scott Martel <tonsit@gmail.com>
2019-12-08 20:27:29 -0800

    Create all remaining CRUD actions

--- collection.js
+++ collection.js
@@ -10,7 +10,12 @@ const table = process.env.COLLECTION_TABLE;
 module.exports.create = async event => {
   const body = JSON.parse(event.body);
   const movie = {
-    title: body.title
+    title: body.title,
+    year: body.year,
+    format: body.format,
+    length: body.length,
+    year: body.year,
+    rating: body.rating
   };
   const params = {
     TableName: table,
@@ -22,16 +27,111 @@ module.exports.create = async event => {
   
   try {
     const result = await db.put(params).promise();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params, result),
-    };
-
+    return successResponse(params, result);
+  
   } catch (error) {
-    return {
-      statusCode: 400,
-      error: error.stack
-    };
+    return errorResponse(error);
 
   }
 };
+
+module.exports.search = async event => {
+  const body = JSON.parse(event.body);
+  const params = {
+    TableName: table,
+  }
+  
+  try {
+    const result = await db.scan(params).promise();
+    return successResponse(params, result);
+  
+  } catch (error) {
+    return errorResponse(error);
+  
+  }
+};
+
+module.exports.read = async event => {
+  const body = JSON.parse(event.body);
+  const params = {
+    TableName: table,
+    Key: {
+      id: {
+        S: body.id
+      }
+    }
+  }
+  
+  try {
+    const result = await db.query(params).promise();
+    return successResponse(params, result);
+  
+  } catch (error) {
+    return errorResponse(error);
+  
+  }
+};
+
+module.exports.update = async event => {
+  const body = JSON.parse(event.body);
+  const movie = {
+    title: body.title
+  };
+  const params = {
+    TableName: table,
+    Item: {
+      id: body.id,
+      details: movie
+    }
+  }
+  
+  try {
+    const result = await db.put(params).promise();
+    return successResponse(params, result);
+
+  } catch (error) {
+    return errorResponse(error);
+
+  }
+};
+
+
+module.exports.delete = async event => {
+  const body = JSON.parse(event.body);
+
+  const params = {
+    TableName: table,
+    Key: {
+      id: {
+        S: body.id
+      }
+    }
+  }
+  
+  try {
+    const result = await db.delete(params).promise();
+    return successResponse(params, result, 204);
+
+  } catch (error) {
+    return errorResponse();
+
+  }
+};
+
+function errorResponse(error, code = 400) {
+  return {
+    statusCode: code,
+    error: error.stack
+  };
+}
+
+function successResponse(params, result, code = 200) {
+  return {
+    statusCode: code,
+    body: JSON.stringify(params, result),
+  };
+}
+
+
+
+
